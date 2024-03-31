35b1922de96efc9e94cd2ffb48406357ba494285
yonraz <yraz530@gmail.com>
2024-03-31 16:23:11 +0300

    game service add validation to join game

--- TalkBackServer/TalkBackServer.Game/controllers/game.controller.js
+++ TalkBackServer/TalkBackServer.Game/controllers/game.controller.js
@@ -1,5 +1,8 @@
 import { socketEmit } from "../app.js";
-import { existsInMap as existsInUsernameToSocketIdMap,  getGameId } from "../utils/utils.js";
+import {
+  existsInMap as existsInUsernameToSocketIdMap,
+  getGameId,
+} from "../utils/utils.js";
 
 export const usernameToSocketIdMap = {};
 
@@ -11,17 +14,29 @@ export async function userJoin(req, res, next) {
     if (!username || !opponent || socketId === "")
       return res.status(404).send("Bad request");
     const gameId = getGameId(username, opponent);
-    if (openGames[gameId] && openGames[gameId].firstPlayer !== "") {
+    console.log("entering " + gameId + `Date is ${Date.now()}`);
+    if (
+      openGames[gameId] &&
+      openGames[gameId].onlineUsers &&
+      openGames[gameId].onlineUsers[username] &&
+      openGames[gameId].onlineUsers[opponent]
+    ) {
       return res.status(404).send("Game is currently on");
     }
-    openGames[gameId] = { firstPlayer: "" };
+    openGames[gameId] = {
+      firstPlayer: "",
+      onlineUsers: {
+        [username]: true,
+      },
+    };
     usernameToSocketIdMap[username] = socketId;
     if (!usernameToSocketIdMap[opponent]) return res.status(200);
 
     socketEmit("user-connection", "", usernameToSocketIdMap[opponent]);
     res.status(200).send({ success: true });
   } catch (err) {
-    res.status(500).send("Internal server error");
+    console.log(err);
+    res.status(500).send(err);
   }
 }
 
@@ -29,10 +44,9 @@ export async function endGame(req, res, next) {
   try {
     const { username, opponent } = req.body;
     if (!username || !opponent) return res.status(404).send("Bad request");
-    if (!areUsersConnected(username, opponent))
-      return res.status(404).send("username or opponent not connected");
     const gameId = getGameId(username, opponent);
-    openGames[gameId] = null;
+    console.log("exiting " + gameId);
+    delete openGames[gameId];
     return res.sendStatus(200);
   } catch (err) {
     return res.status(500).send("Internal server error");
@@ -52,7 +66,6 @@ export async function startGame(req, res, next) {
   try {
     const { gameObject, username, opponent } = req.body;
     if (!gameObject) {
-      console.log("no game");
       return res.status(404).send("one or more fields is invalid");
     }
     if (!areUsersConnected(username, opponent)) {
@@ -89,8 +102,6 @@ export async function select(req, res, next) {
 
     return res.sendStatus(200);
   } catch (err) {
-    console.error("Error in select function:", err);
-
     if (err instanceof SyntaxError) {
       return res.status(400).send("Invalid JSON format");
     } else if (err instanceof TypeError) {
@@ -116,10 +127,8 @@ export async function notifyChangeTurn(req, res, next) {
 
 export async function rollDice(req, res, next) {
   try {
-    console.log(req.body);
     const { turnJson, username, opponent } = req.body;
     if (!turnJson) {
-      console.log("wat");
       return res.status(404).send("one or more fields is invalid");
     }
     if (!areUsersConnected(username, opponent)) {
@@ -151,7 +160,6 @@ export async function getFirstPlayer(req, res, next) {
     setFirstPlayer(gameId, result);
     return res.status(200).json({ result });
   } catch (err) {
-    console.log(err);
     return res.status(500).send("Internal server error");
   }
 }
